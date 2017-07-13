15f3dbdafd027a8c4f27a4b2f4df3047a31e6bc9
maryfranks <maryfranks@gmail.com>
2017-07-13 17:08:40 -0400

    finishes crm pages and styling

--- crm.rb
+++ crm.rb
@@ -13,7 +13,11 @@ end
 get '/contacts/:id' do
   params[:id]
   @contact = Contact.find_by({id: params[:id].to_i})
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 get '/about' do
