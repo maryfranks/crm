f3b23102d7f0e211e10d331ea25e391720c3d5d5
maryfranks <maryfranks@gmail.com>
2017-07-14 14:24:04 -0400

    adds form to new.erb and updates methods on crm.rb

--- crm.rb
+++ crm.rb
@@ -28,6 +28,16 @@ get '/about' do
   erb :about
 end
 
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
+end
+
 after do
   ActiveRecord::Base.connection.close
 end
